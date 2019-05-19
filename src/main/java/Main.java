c94e41a77742bc32886dd74f4c2e7472923e4b93
lucas <lucas.salse@gmail.com>
2019-05-19 09:27:46 -0300

    Tercer commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,8 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo hdp") ; //Concepcion alvarado el ERIK
-        System.out.println("HOLAAAA") ;
+        System.out.println("HOLAAAA") ; 
+        System.out.println("hola") ;
         
     }
     
